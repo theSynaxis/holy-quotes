01f23336efd30d33fd2edc63317c2955723a0dfd
Moses Cosme <mosesmaxcosme@gmail.com>
2021-06-28 11:05:03 -0500

    feat(server): add author seed
    This commit will add an author seed and add author tables.

--- src/server/data/migrations/20210625221603_create_authors.js
+++ src/server/data/migrations/20210625221603_create_authors.js
@@ -2,6 +2,16 @@
 exports.up = function (knex) {
   return knex.schema.createTable('authors', (tbl) => {
     tbl.increments();
+    tbl.string('name');
+    tbl.string('title');
+    tbl.string('born');
+    tbl.string('died');
+    tbl.boolean('isBC');
+    tbl.string('feast_day');
+    tbl.text('life');
+    tbl.timestamp('created_at').defaultTo(knex.fn.now());
+    tbl.timestamp('modified_at').defaultTo(knex.fn.now());
+    tbl.boolean('is_deleted').defaultTo(false);
   });
 };
 
